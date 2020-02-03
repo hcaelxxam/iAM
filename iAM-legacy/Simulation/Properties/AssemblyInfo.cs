47ca7b8592634c071ad2372fa5590c03121afea3
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2020-02-03 11:15:33 -0600

    changed the log config file back to log4net.config

--- iAM-legacy/Simulation/Properties/AssemblyInfo.cs
+++ iAM-legacy/Simulation/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Logging configuration
-[assembly: log4net.Config.XmlConfigurator(ConfigFile = "logSimulation.config")]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
